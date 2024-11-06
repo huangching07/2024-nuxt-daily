4c88c9c70c894ad830a05927f66c1674f2a7b08b
Ching Huang <huangching.07@gmail.com>
2024-11-06 21:48:28 +0800

    add index page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>Page: 首頁</h1>
+    目前路由的路徑 - route.fullPath 為：{{ route.fullPath }}
+  </div>
+</template>
+
+<style scoped></style>
