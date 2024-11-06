2a6cb496f1d3e3587a14c014d8105e2d00cc6aa2
Ching Huang <huangching.07@gmail.com>
2024-11-06 22:23:19 +0800

    add global style

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,16 @@ const route = useRoute();
   <div>
     <h1>Page: 首頁</h1>
     目前路由的路徑 - route.fullPath 為：{{ route.fullPath }}
+    <br />
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-success">Sucess</button>
+    <button class="btn btn-danger">Danger</button>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-info">Info</button>
+    <button class="btn btn-light">Light</button>
+    <button class="btn btn-dark">Dark</button>
+    <button class="btn btn-link">Link</button>
   </div>
 </template>
 
